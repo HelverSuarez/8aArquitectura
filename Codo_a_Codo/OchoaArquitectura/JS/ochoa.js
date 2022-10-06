7d09559712e6c74e338090741f2924c587c91571
CinGoddio <cintiagoddio@gmail.com>
2022-10-06 17:55:16 -0300

    elimine las secciones que no usaremos

--- Codo_a_Codo/OchoaArquitectura/JS/ochoa.js
+++ Codo_a_Codo/OchoaArquitectura/JS/ochoa.js
@@ -1,8 +1,8 @@
-document.getElementsById("idheader").innerHTML=`<nav>
+document.getElementsById("idheader").innerHTML=`<div class="logo"><img src="IMAGENES/logo.png" alt="Logo Ochoa Arquitectura" width="100"></div>
+<nav>
 <br>
      <a class="nav" href="index.html">Home</a>
      <a class="nav" href="obras-insitucionales.html">Obras Institucionales</a>
-     <a class="nav" href="viviendas.html">Viviendas</a>
      <a class="nav" href="servicios.html">Servicios</a>
      <a class="nav" href="contacto.html">Contacto</a>
 <br>
@@ -11,4 +11,3 @@ document.getElementsById("idheader").innerHTML=`<nav>
 document.getElementById("idfooter").innerHTML=`<p><a id="mail" href="mailto:estudio-arquitectura@federico-ochoa.com">estudio-arquitectura@federico-ochoa.com</a> <br> +54 9 3544 60 9336</p>
 <a href="#" target="_blank"><i id="redsoc" class="fa-brands fa-facebook-f"></i></a>
 <a href="#" target="_blank"><i id="redsoc" class="fa-brands fa-instagram"></i></a>`
-
